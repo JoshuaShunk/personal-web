4434f9e340ec07f4a975052d06039f585ae795bf
JoshuaShunk <hockeyshunk04@gmail.com>
2024-04-19 21:22:19 -0700

    Build fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <style>{`body { display: none; }`}</style>
-        <script nonce={nonce} src="/set-theme.js"></script>{" "}
+        <script nonce={nonce} async src="/set-theme.js"></script>{" "}
         {/* Secured by nonce */}
       </head>
       <body
