ddaa5a0153ade1c02e3e9b4f68a68439a7e74b79
JoshuaShunk <hockeyshunk04@gmail.com>
2024-04-19 21:37:52 -0700

    Attempt to fix build again

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import "@radix-ui/themes/styles.css";
-import type { ReactNode } from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 import { ThemeProvider } from "next-themes";
 import NavBar from "./components/navbar/NavBar";
+import type { ReactNode, FC } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +18,7 @@ export const metadata = {
   description: "Personal website created to show off my projects and resume.",
 };
 
-const RootLayout: React.FC<RootLayoutProps> = ({ children, nonce }) => {
+const RootLayout: FC<RootLayoutProps> = ({ children, nonce }) => {
   return (
     <html lang="en">
       <head>
