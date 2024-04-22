180d39c921fe16a04e4f70c0a66660c87784fc31
JoshuaShunk <hockeyshunk04@gmail.com>
2024-04-22 11:21:49 -0700

    Attempt to fix caching issue

--- app/api/blog/route.ts
+++ app/api/blog/route.ts
@@ -3,17 +3,13 @@ import { connectDB, disconnectDB } from "../../../utils/databse";
 
 import { NextResponse } from "next/server";
 
+export const revalidate = 60;
+
 export const GET = async (req: Request, res: NextResponse) => {
     try {
         await connectDB();
         const post = await prisma.post.findMany();
-        // Create a NextResponse object with the data
-        const response = NextResponse.json({ message: "Data fetched successfully", post });
-        
-        // Set the Cache-Control header to 'no-store'
-        response.headers.set('Cache-Control', 'no-store');
-
-        return response;
+        return NextResponse.json({message: "Data fetched successfully", post}, {status: 200})
     } catch (err) {
         return NextResponse.json({message: "Error fetching data", err}, {status: 500})
     } finally{
